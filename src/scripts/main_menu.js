c7e9d11faed91d93f53515d978f4efc52da321a8
Jiamin-Zou <jiaminz622@gmail.com>
2023-05-11 13:07:07 -0400

    Added CSS styling, still wip, save point

--- src/scripts/main_menu.js
+++ src/scripts/main_menu.js
@@ -4,40 +4,60 @@ import GameView from "./game_view.js";
 class MenuHandler {
     constructor() {
         this.game, this.gameView;
-        this.startMenu = document.querySelector("#game-menu");
+
         this.canvas = document.querySelector("#canvas");
         this.ctx = this.canvas.getContext("2d")
+
         this.modal = document.querySelector("#gameModal");
+        this.closeModal = document.querySelector(".close-game-menu")
         this.modal.style.display = "flex";
-        this.startGameBtn = document.querySelector("#game-start-btn");
-        this.difficultySelector = document.querySelector(".game-diff-ctn");
-        this.startGame = this.startGame.bind(this);
+        this.openModal = document.querySelector(".fa-circle-info")
+        this.difficultySelector = document.querySelector(".game-diff-levels");
+        
         this.bgm = document.getElementById("gameBGM");
-        this.audioToggle = document.querySelector(".instructions")
+        this.audioToggle = document.querySelector(".fa-play")
+
+        this.startGameBtn = document.querySelector("#game-start-btn");
+        this.startGame = this.startGame.bind(this);
         this.bindEventListeners();
     }
 
     bindEventListeners() {
         this.startGameBtn.addEventListener("click", ()=> {
             this.modal.style.display = "none";
-            // debugger
                 this.startGame()
         })
 
+        
+        this.closeModal.addEventListener("click", ()=> {
+            this.modal.style.display = "none";
+        })
+
+
+
         this.difficultySelector.addEventListener("click", (e)=> {
-            const selected = document.querySelector(".difficulty-selected")
-            if (selected !== e.target) {
-                selected.className = "diff-selections-lv";
-                e.target.classList.add("difficulty-selected")
+            if (e.target.className === "diff-selections-lv") {
+                const selected = document.querySelector(".difficulty-selected")
+                if (selected !== e.target) {
+                    selected.className = "diff-selections-lv";
+                    e.target.classList.add("difficulty-selected")
+                }
             }
         })
         
-        this.audioToggle.addEventListener("click", (e)=> {
-            if(this.bgm .paused) {
+        this.audioToggle.addEventListener("click", ()=> {
+            let audioControl = document.querySelector(".audio-display")
+            if(this.bgm.paused) {
                 this.bgm.play();
+                audioControl.innerText = "Pause"
             } else {
                 this.bgm.pause();
+                audioControl.innerText = "Play"
             }
+        })
+
+        this.openModal.addEventListener("click", () => {
+            this.modal.style.display = "flex";
         })
         
     }
@@ -46,16 +66,10 @@ class MenuHandler {
         const difficulty = document.querySelector(".difficulty-selected").innerText;
         this.modal.style.display = "none";
         if (this.startGameBtn.innerText === "Start Game") {
-            // debugger
             this.game = new Game(this.canvas, difficulty);
             this.gameView = new GameView(this.game, this.ctx);
             this.gameView.start();
         } else {
-            // debugger
-            // debugger
-            // this.game = new Game(this.canvas, difficulty);
-            // this.gameView = new GameView(this.game, this.ctx);
-            // this.gameView.start();
             this.gameView.restart(difficulty);
         }
     }
